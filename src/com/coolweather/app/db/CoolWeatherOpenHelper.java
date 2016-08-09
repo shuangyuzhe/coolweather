fc621275bb6f4d67c831f360bc36daebf129a395
meng <272993242@qq.com>
2016-08-09 17:47:40 +0800

    完成遍历省市县三级列表的功能

--- src/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -22,7 +22,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 			+"id integer primary key autoincrement,"
 			+"county_name text,"
 			+"county_code text,"
-			+"city_id integer";
+			+"city_id integer)";
 	
 
 	public CoolWeatherOpenHelper(Context context, String name,
